d3a52216b42514ef0ca0abecb5339743e580d4e2
Tom Kirchner <tjk@amazon.com>
2020-02-21 14:51:39 -0800

    Rename workspaces directory to 'sources', package to 'os'
    workspaces was originally a set of Cargo workspaces, but we now build them
    together because it's much faster to build.  This renames the 'workspaces'
    directory to the clearer 'sources' because it contains our source code.  The
    'workspaces' package is renamed to 'os' to represent that it contains our
    top-level / userspace OS components.

---
+++ sources/models/README.md
@@ -0,0 +1,55 @@
+# models
+
+Current version: 0.1.0
+
+## API models
+
+Bottlerocket has different variants supporting different features and use cases.
+Each variant has its own set of software, and therefore needs its own configuration.
+We support having an API model for each variant to support these different configurations.
+
+Each model defines a top-level `Settings` structure.
+It can use pre-defined structures inside, or custom ones as needed.
+
+This `Settings` essentially becomes the schema for the variant's data store.
+`apiserver::datastore` offers serialization and deserialization modules that make it easy to map between Rust types and the data store, and thus, all inputs and outputs are type-checked.
+
+At the field level, standard Rust types can be used, or ["modeled types"](src/modeled_types) that add input validation.
+
+Default values are specified in [defaults.toml](defaults.toml) and can be overridden by each variant.
+
+The `#[model]` attribute on Settings and its sub-structs reduces duplication and adds some required metadata; see [its docs](model-derive/) for details.
+
+### aws-k8s: Kubernetes
+
+* [Model](src/aws-k8s/mod.rs)
+* [Overridden defaults](src/aws-k8s/override-defaults.toml)
+
+### aws-dev: Development build
+
+* [Model](src/aws-dev/mod.rs)
+* [Overridden defaults](src/aws-dev/override-defaults.toml)
+
+## This directory
+
+We use `build.rs` to symlink the proper API model source code for Cargo to build.
+We determine the "proper" model by using the `VARIANT` environment variable.
+
+If a developer is doing a local `cargo build`, they need to set `VARIANT`.
+
+When building with the Bottlerocket build system, `VARIANT` is based on `BUILDSYS_VARIANT` from the top-level `Makefile.toml`, which can be overridden on the command line with `cargo make -e BUILDSYS_VARIANT=bla`.
+
+Note: when building with the build system, we can't create the symlink in the source directory during a build - the directories are owned by `root`, but we're `builder`.
+We can't use a read/write bind mount with current Docker syntax.
+To get around this, in the top-level `Dockerfile`, we mount a "cache" directory at `src/variant` that we can modify, and create a `current` symlink inside.
+The code in `src/lib.rs` then imports the requested model using `variant/current`.
+
+Note: for the same reason, we symlink `variant/mod.rs` to `variant_mod.rs`.
+Rust needs a `mod.rs` file to understand that a directory is part of the module structure, so we have to have `variant/mod.rs`.
+`variant/` is the cache mount that starts empty, so we have to store the file elsewhere and link it in.
+
+Note: all models share the same `Cargo.toml`.
+
+## Colophon
+
+This text was generated from `README.tpl` using [cargo-readme](https://crates.io/crates/cargo-readme), and includes the rustdoc from `src/lib.rs`.
---
+++ sources/models/src/lib.rs
@@ -0,0 +1,151 @@
+/*!
+# API models
+
+Bottlerocket has different variants supporting different features and use cases.
+Each variant has its own set of software, and therefore needs its own configuration.
+We support having an API model for each variant to support these different configurations.
+
+Each model defines a top-level `Settings` structure.
+It can use pre-defined structures inside, or custom ones as needed.
+
+This `Settings` essentially becomes the schema for the variant's data store.
+`apiserver::datastore` offers serialization and deserialization modules that make it easy to map between Rust types and the data store, and thus, all inputs and outputs are type-checked.
+
+At the field level, standard Rust types can be used, or ["modeled types"](src/modeled_types) that add input validation.
+
+Default values are specified in [defaults.toml](defaults.toml) and can be overridden by each variant.
+
+The `#[model]` attribute on Settings and its sub-structs reduces duplication and adds some required metadata; see [its docs](model-derive/) for details.
+
+## aws-k8s: Kubernetes
+
+* [Model](src/aws-k8s/mod.rs)
+* [Overridden defaults](src/aws-k8s/override-defaults.toml)
+
+## aws-dev: Development build
+
+* [Model](src/aws-dev/mod.rs)
+* [Overridden defaults](src/aws-dev/override-defaults.toml)
+
+# This directory
+
+We use `build.rs` to symlink the proper API model source code for Cargo to build.
+We determine the "proper" model by using the `VARIANT` environment variable.
+
+If a developer is doing a local `cargo build`, they need to set `VARIANT`.
+
+When building with the Bottlerocket build system, `VARIANT` is based on `BUILDSYS_VARIANT` from the top-level `Makefile.toml`, which can be overridden on the command line with `cargo make -e BUILDSYS_VARIANT=bla`.
+
+Note: when building with the build system, we can't create the symlink in the source directory during a build - the directories are owned by `root`, but we're `builder`.
+We can't use a read/write bind mount with current Docker syntax.
+To get around this, in the top-level `Dockerfile`, we mount a "cache" directory at `src/variant` that we can modify, and create a `current` symlink inside.
+The code in `src/lib.rs` then imports the requested model using `variant/current`.
+
+Note: for the same reason, we symlink `variant/mod.rs` to `variant_mod.rs`.
+Rust needs a `mod.rs` file to understand that a directory is part of the module structure, so we have to have `variant/mod.rs`.
+`variant/` is the cache mount that starts empty, so we have to store the file elsewhere and link it in.
+
+Note: all models share the same `Cargo.toml`.
+*/
+
+// "Modeled types" are types with special ser/de behavior used for validation.
+pub mod modeled_types;
+
+// The "variant" module is just a directory where we symlink in the user's requested build
+// variant; each variant defines a top-level Settings structure and we re-export the current one.
+mod variant;
+pub use variant::Settings;
+
+// Below, we define common structures used in the API surface; specific variants build a Settings
+// structure based on these, and that's what gets exposed via the API.  (Specific variants' models
+// are in subdirectories and linked into place by build.rs at variant/current.)
+
+use model_derive::model;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::net::Ipv4Addr;
+
+use crate::modeled_types::{
+    KubernetesClusterName, KubernetesLabelKey, KubernetesLabelValue, KubernetesTaintValue,
+    SingleLineString, Url, ValidBase64,
+};
+
+// Kubernetes related settings. The dynamic settings are retrieved from
+// IMDS via Sundog's child "Pluto".
+#[model]
+struct KubernetesSettings {
+    // Settings we require the user to specify, likely via user data.
+    cluster_name: KubernetesClusterName,
+    cluster_certificate: ValidBase64,
+    api_server: Url,
+    node_labels: HashMap<KubernetesLabelKey, KubernetesLabelValue>,
+    node_taints: HashMap<KubernetesLabelKey, KubernetesTaintValue>,
+
+    // Dynamic settings.
+    max_pods: u32,
+    cluster_dns_ip: Ipv4Addr,
+    node_ip: Ipv4Addr,
+    pod_infra_container_image: SingleLineString,
+}
+
+// Updog settings. Taken from userdata. The 'seed' setting is generated
+// by the "Bork" settings generator at runtime.
+#[model]
+struct UpdatesSettings {
+    metadata_base_url: Url,
+    target_base_url: Url,
+    seed: u32,
+}
+
+#[model]
+struct ContainerImage {
+    source: Url,
+    enabled: bool,
+    superpowered: bool,
+}
+
+// NTP settings
+#[model]
+struct NtpSettings {
+    time_servers: Vec<Url>,
+}
+
+// Platform-specific settings
+#[model]
+struct AwsSettings {
+    region: SingleLineString,
+}
+
+///// Internal services
+
+// Note: Top-level objects that get returned from the API should have a "rename" attribute
+// matching the struct name, but in kebab-case, e.g. ConfigurationFiles -> "configuration-files".
+// This lets it match the datastore name.
+// Objects that live inside those top-level objects, e.g. Service lives in Services, should have
+// rename="" so they don't add an extra prefix to the datastore path that doesn't actually exist.
+// This is important because we have APIs that can return those sub-structures directly.
+
+pub type Services = HashMap<String, Service>;
+
+#[model(add_option = false, rename = "")]
+struct Service {
+    configuration_files: Vec<SingleLineString>,
+    restart_commands: Vec<String>,
+}
+
+pub type ConfigurationFiles = HashMap<String, ConfigurationFile>;
+
+#[model(add_option = false, rename = "")]
+struct ConfigurationFile {
+    path: SingleLineString,
+    template_path: SingleLineString,
+}
+
+///// Metadata
+
+#[model(add_option = false, rename = "metadata")]
+struct Metadata {
+    key: SingleLineString,
+    md: SingleLineString,
+    val: toml::Value,
+}
