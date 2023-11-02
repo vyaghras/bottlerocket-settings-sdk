bottlerocket-os/bottlerocket

settings: use motd settings extension in models

sources/models/src/aws-dev/mod.rs
@@ -13,7 +13,7 @@ use crate::{
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: motd::MotdV1,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,
     bootstrap_containers: HashMap<Identifier, BootstrapContainer>,

sources/models/src/aws-k8s-1.28-nvidia/mod.rs
@@ -14,7 +14,7 @@ use std::collections::HashMap;
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: motd::MotdV1,
     kubernetes: KubernetesSettings,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,

sources/models/src/metal-dev/mod.rs
@@ -12,7 +12,7 @@ use crate::{
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: motd::MotdV1,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,
     bootstrap_containers: HashMap<Identifier, BootstrapContainer>,

vyaghras/bottlerocket-settings-sdk

sources/models/src/aws-dev/mod.rs
@@ -13,7 +13,7 @@ use crate::{
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: settings_extension_motd::MotdV1,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,
     bootstrap_containers: HashMap<Identifier, BootstrapContainer>,

sources/models/src/aws-k8s-1.28-nvidia/mod.rs
@@ -14,7 +14,7 @@ use std::collections::HashMap;
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: settings_extension_motd::MotdV1,
     kubernetes: KubernetesSettings,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,

sources/models/src/metal-dev/mod.rs
@@ -12,7 +12,7 @@ use crate::{
 // that uses its name in serialization; internal structures use the field name that points to it
 #[model(rename = "settings", impl_default = true)]
 struct Settings {
-    motd: String,
+    motd: settings_extension_motd::MotdV1,
     updates: UpdatesSettings,
     host_containers: HashMap<Identifier, HostContainer>,
     bootstrap_containers: HashMap<Identifier, BootstrapContainer>,