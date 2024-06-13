fefcd0eaae03dd5b5dead54371cb4d6b69fa2dce
Sean P. Kelly <seankell@amazon.com>
2024-06-13 23:09:55 +0000

    refactor: organize symbols in settings-models
    This change also merges the settings-models and SDK workspaces.

---
+++ bottlerocket-settings-models/settings-models/src/boot.rs
@@ -0,0 +1,24 @@
+//! Boot config settings structures.
+use bottlerocket_model_derive::model;
+use bottlerocket_modeled_types::{BootConfigKey, BootConfigValue};
+use std::collections::HashMap;
+
+// Kernel boot settings
+#[model(impl_default = true)]
+struct BootSettings {
+    reboot_to_reconcile: bool,
+    #[serde(
+        alias = "kernel",
+        rename(serialize = "kernel"),
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    kernel_parameters: HashMap<BootConfigKey, Vec<BootConfigValue>>,
+    #[serde(
+        alias = "init",
+        rename(serialize = "init"),
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    init_parameters: HashMap<BootConfigKey, Vec<BootConfigValue>>,
+}
