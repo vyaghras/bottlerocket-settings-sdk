3c9b9638b21d32eb89dccad031574f88c55a91d8
Sean P. Kelly <seankell@amazon.com>
2024-06-14 22:05:39 +0000

    feat: use env-logger in motd extension

--- bottlerocket-settings-models/settings-extensions/motd/src/main.rs
+++ bottlerocket-settings-models/settings-extensions/motd/src/main.rs
@@ -3,6 +3,8 @@ use settings_extension_motd::MotdV1;
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
+    env_logger::init();
+
     match LinearMigratorExtensionBuilder::with_name("motd")
         .with_models(vec![BottlerocketSetting::<MotdV1>::model()])
         .build()
